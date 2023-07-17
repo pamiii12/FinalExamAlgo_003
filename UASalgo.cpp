e0019a336ceb9fcb7ee2f626beca73f7076a5685
pamiii12 <fahmiadityaa03@gmail.com>
2023-07-17 13:45:14 +0700

    final

--- UASalgo.cpp
+++ UASalgo.cpp
@@ -1,20 +1,118 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//isi disini
-const int MAX_MAHASISWA = 10;
+
+struct Node {
+	int noMhs;
+	string name;
+	string Jurusan;
+	int TahunMasuk;
+	Node* next;
+};
+
+const int MAX_MAHASISWA = 100;
 int NIM[MAX_MAHASISWA];
 string nama[MAX_MAHASISWA];
 int tahunMasuk[MAX_MAHASISWA];
 int jumlahMahasiswa = 0;
-//isi disini
-void tambahMahasiswa();
-//isi disini
-void tampilkanSemuaMahasiswa();
-//isi disini
-void algorithmacariMahasiswaByNIM();
-//isi disini
-void algorithmaSortByTahunMasuk();
+
+Node* START = NULL;
+ 
+void tambahMahasiswa() {
+	int nim;
+	string nama;
+	string jurusan;
+	int thnMasuk;
+	Node* nodeBaru = new Node();
+	cout << "NIM         : ";
+	cin >> nim;
+	cout << "Nama        : ";
+	cin >> nama;
+	cout << "Jurusan     : ";
+	cin >> jurusan;
+	cout << "Tahun Masuk : ";
+	cin >> thnMasuk;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
+}
+
+void tampilkanSemuaMahasiswa() {
+	Node* currentNode = START;
+	while (currentNode != NULL) {
+		cout << "NIM : " << currentNode->noMhs 
+			<< "', Nama : " << currentNode->name 
+			<< "', Jurusan : " << currentNode->Jurusan
+			<< "', Tahun Masuk : " << currentNode->TahunMasuk << endl;
+		currentNode = currentNode->next;
+	}
+}
+
+void algorithmacariMahasiswaByNIM() {
+	int nim;
+	cout << "Masukkan NIM : ";
+	cin >> nim;
+	Node* currentNode = START;
+	while (currentNode != NULL) {
+		if (currentNode->noMhs == nim) {
+			cout << "NIM : " << currentNode->noMhs 
+				<< ", Nama : " << currentNode->name 
+				<< "', Jurusan : " << currentNode->Jurusan
+				<< "', Tahun Masuk : " << currentNode->TahunMasuk << endl;
+			return;
+		}
+		currentNode = currentNode->next;
+	}
+	cout << "Data tidak ditemukan" << endl;
+}
+
+
+void algorithmaSortByTahunMasuk() {
+	int tahunMasuk;
+	cout << "tampilkan Tahun Masuk nya : ";
+	cin >> tahunMasuk;
+	Node* currentNode = START;
+	while (currentNode != NULL) {
+		if (currentNode->TahunMasuk == tahunMasuk) {
+			cout << "NIM : " << currentNode->noMhs
+				<< ", Nama : " << currentNode->name
+				<< "', Jurusan : " << currentNode->Jurusan 
+				<< "', Tahun Masuk : " << currentNode->TahunMasuk << endl;
+			return;
+		}
+		currentNode = currentNode->next;
+	}
+	cout << "Data tidak ditemukan" << endl;
+}
+
 int main() {
 	int pilihan;
 	do {
@@ -29,16 +127,16 @@ int main() {
 		cin.ignore();
 		switch (pilihan) {
 		case 1:
-			//isi disini
+			tambahMahasiswa();
 			break;
 		case 2:
-			//isi disini
+			tampilkanSemuaMahasiswa();
 			break;
 		case 3:
-			//isi disini
+			algorithmacariMahasiswaByNIM();
 			break;
 		case 4:
-			//isi disini
+			algorithmaSortByTahunMasuk();
 			break;
 		case 5:
 			cout << "Terima kasih! Program selesai." << endl;
@@ -50,3 +148,11 @@ int main() {
 	} while (pilihan != 5);
 	return 0;
 }
+
+
+/* 2. singlelinkedlist
+   3. Elements di stack dimasukkan dan dihapus berbasis LIFO(Last In First Ou)
+      sedangkan QUEUE Menggunakan FIFO (First In First Out)
+   4. jika ingin memakai Elements yg berbasis LIFO (Last In First Out)
+   5. a. Sebanyak 9 Struktur
+      b. Menggunakan metode inOrder*/
